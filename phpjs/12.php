3b460c4a9e268faa524bbc000b060dd1962c1abc
GoldPenguin <sanghyup1993@gmail.com>
2018-11-09 15:22:30 +0900

    Add files via upload

---
+++ phpjs/12.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script>
+      function a(){
+        document.write("hello JS function");
+      }
+      a();
+    </script>
+    <h2>php</h2>
+    <?php
+      function a(){
+        echo "hello function";
+      }
+      a();
+     ?>
+  </body>
+</html>
